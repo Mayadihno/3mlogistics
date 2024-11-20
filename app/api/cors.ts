dd724f367ad35d3f507eafa38890441f41aa9dba
Maya Tunde Olawale <mayadihno@gmail.com>
2024-11-20 17:39:43 +0100

    fix; cors type fix

--- app/api/cors.ts
+++ app/api/cors.ts
@@ -15,7 +15,7 @@ function runMiddleware(
   fn: Function
 ) {
   return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
+    fn(req, res, (result: unknown) => {
       if (result instanceof Error) {
         return reject(result);
       }
